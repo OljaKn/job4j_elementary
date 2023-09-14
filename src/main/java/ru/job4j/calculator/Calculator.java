0cbfee00b52fc38345bd826f7534586ae55d1ce1
HOME <olga.yasemchik@bk.ru>
2023-09-14 15:02:54 +0300

    вызов статического метода внутри метода

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,11 +1,32 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
+        plus(100, 500);
+        plus(4, 2);
+        plus(3, 5);
+        minus(7, 3);
+        minus(2, 6);
+
         int one = 1;
         int two = 2;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
+        int result = one + two;
+        System.out.println(result);
+        one = 10;
+        two = 11;
+        result = one + two;
+        System.out.println(result);
 
         int six = 6;
         int four = 4;
@@ -18,5 +39,10 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
     }
 }
