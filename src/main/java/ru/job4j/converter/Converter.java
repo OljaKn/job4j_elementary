42f1772cb36190dc7a0e6e66590436cabf30f965
HOME <olga.yasemchik@bk.ru>
2023-09-15 14:31:56 +0300

    протестировать метод Cjnverter

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,9 +12,21 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(1000);
-        float dollar = Converter.rubleToDollar(500);
-        System.out.println("1000 rubles are " + euro + " euro.");
-        System.out.println("500 rubles are " + dollar + " dollar.");
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(600);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("600 rubles are " + dollar + " dollar.");
+
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        float in1 = 600;
+        float expected1 = 10;
+        float out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("600 rubles are 10. Test result : " + passed1);
     }
 }
