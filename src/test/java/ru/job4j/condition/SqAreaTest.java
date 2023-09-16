31cc1583e331aa2ff88baf14b57cc80160492de6
HOME <olga.yasemchik@bk.ru>
2023-09-16 21:54:06 +0300

    creat test method to check a square

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP8K1Square4() {
+        int expected = 4;
+        int p = 8;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP10K4Square4() {
+        int expected = 4;
+        int p = 10;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
