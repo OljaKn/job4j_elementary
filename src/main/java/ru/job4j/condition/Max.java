95d60dd566f6032576812949cbfabf40595a5557
HOME <olga.yasemchik@bk.ru>
2023-10-06 14:28:05 +0300

    найти max число из 2/3/4 чисел

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,17 +1,27 @@
 package ru.job4j.condition;
 
 public class Max {
-    public  static int max(int left, int right) {
-        int result = left > right ? left : right;
+    public  static int max(int a, int b) {
+        int result = a > b ? a : b;
+        return result;
+    }
+
+    public  static int max(int a, int b, int c) {
+        int result = max(a, b) > c ? max(a, b) : c;
+        return result;
+    }
+
+    public  static int max(int a, int b, int c, int d) {
+        int result = max(a, b, c) > d ? max(a, b, c) : d;
         return result;
     }
 
     public static void main(String[] args) {
         int i = Max.max(10, 5);
         System.out.println(i);
-        int i1 = Max.max(5, 20);
+        int i1 = Max.max(10, 5, 20);
         System.out.println(i1);
-        int i2 = Max.max(5, 5);
+        int i2 = Max.max(10, 5, 20, 6);
         System.out.println(i2);
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -9,10 +9,31 @@ class MaxTest {
 
     @Test
     void whenMax10To5Then10() {
-        int left = 10;
-        int right = 5;
-        int result = Max.max(left, right);
+        int a = 10;
+        int b = 5;
+        int result = Max.max(a, b);
         int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax5To14To10Then14() {
+        int a = 5;
+        int b = 14;
+        int c = 10;
+        int result = Max.max(a, b, c);
+        int expected = 14;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax5To14To10To20Then20() {
+        int a = 5;
+        int b = 14;
+        int c = 10;
+        int d = 20;
+        int result = Max.max(a, b, c, d);
+        int expected = 20;
         assertThat(result).isEqualTo(expected);
     }
 }
